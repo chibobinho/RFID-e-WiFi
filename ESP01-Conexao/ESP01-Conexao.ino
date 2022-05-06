ec0b1616732d1b0b9f8b7a32598b6e2b292fb5a4
Yuri Mitsugui Chiba <44440053866@info.scs>
2022-05-06 11:07:14 -0300

    conectado ao wifi

--- ESP01-Conexao/ESP01-Conexao.ino
+++ ESP01-Conexao/ESP01-Conexao.ino
@@ -1,13 +1,40 @@
+#include <ArduinoWiFiServer.h>
+#include <BearSSLHelpers.h>
+#include <CertStoreBearSSL.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiAP.h>
+#include <ESP8266WiFiGeneric.h>
+#include <ESP8266WiFiGratuitous.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WiFiScan.h>
+#include <ESP8266WiFiSTA.h>
+#include <ESP8266WiFiType.h>
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+#include <WiFiClientSecureBearSSL.h>
+#include <WiFiServer.h>
+#include <WiFiServerSecure.h>
+#include <WiFiServerSecureBearSSL.h>
+#include <WiFiUdp.h>
+
+#include <dummy.h>
+
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
+
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 
-const char* ssid = "meuSSID";
-const char* password = "minhaSenha";
+const char* ssid = "Vermelho-5G";
+const char* password = "Vermelho@132";
 
 void setup(){
 
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
+  WiFi.begin("Vermelho-5G", "Vermelho@132");
 
   while (WiFi.status() != WL_CONNECTED) {
 
@@ -16,21 +43,29 @@ void setup(){
   }
 }
 void loop() {
-  if(WiFi.status()== WL_CONNECTED {
-    HTTPClient http; 
+ 
+  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
 
-    http.begin("http://jsonplaceholder.typicode.com/users"); // DESTINO DA REQUISIÇÃO!!!
-    http.addHeader ("Content-Type", "text/plain"); //CONTENT TYPE
-
-    int httpCode = http.POST("Mensagem do ESP01"); 
-    string payload = http.getString();
-
-    Serial.println(httpCode);
-    Serial.println(payload);
-
-    http.end();
-  }else{
-    Serial.println("Erro na conexão Wifi");
+    WiFiClient client;
+    HTTPClient http;    //Declare object of class HTTPClient
+ 
+    http.begin(client, "http://jsonplaceholder.typicode.com/users");      //Specify request destination
+    http.addHeader("Content-Type", "text/plain");  //Specify content-type header
+ 
+    int httpCode = http.POST("Message from ESP8266");   //Send the request
+    String payload = http.getString();                  //Get the response payload
+ 
+    Serial.println(httpCode);   //Print HTTP return code
+    Serial.println(payload);    //Print request response payload
+ 
+    http.end();  //Close connection
+ 
+  } else {
+ 
+    Serial.println("Error in WiFi connection");
+ 
   }
-  delay(30000);
+ 
+  delay(30000);  //Send a request every 30 seconds
+ 
 }
